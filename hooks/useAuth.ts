77d0c4313361d702f3125304436c122fe33df3f5
unknown <iamnonso@voltech.com.ng>
2025-04-05 18:05:36 +0200

    @feat/login-route

---
+++ hooks/useAuth.ts
@@ -0,0 +1,39 @@
+
+import { useState } from "react";
+import { authService, LoginResponse } from "../services/api/auth";
+import { useRouter } from "next/router";
+
+export const useAuth = () => {
+  const [error, setError] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+  const router = useRouter();
+
+  const login = async (email: string, password: string) => {
+    setError("");
+    setIsLoading(true);
+
+    try {
+      const response = await authService.login({
+        userid: email,
+        password,
+      });
+
+      if (response.token) {
+        localStorage.setItem("userData", JSON.stringify(response.data));
+        document.cookie = `token=${response}; path=/`;
+        router.push("/dashboard");
+        return true;
+      } else {
+        setError(response.error || "Login failed, email or password is wrong!");
+        return false;
+      }
+    } catch (err) {
+      setError("You are not allowed to login. Please try again");
+      return false;
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  return { login, error, isLoading };
+};

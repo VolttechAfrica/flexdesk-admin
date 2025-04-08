8cf53c562805a3a27effc44522148901406ed649
Emmanuel <chinonso.madugba@gmail.com>
2025-04-08 18:57:13 +0200

    fix: fixed environmental variables issue

--- config/constants.ts
+++ config/constants.ts
@@ -1,3 +1,3 @@
-export const API_URL = process.env.PUBLIC_API_URL
-export const APP_NAME = process.env.APP_NAME || "FlexTest";
-export const MOCKARO_KEY = process.env.MOCKARO_KEY
+export const API_URL = process.env.NEXT_PUBLIC_API_URL
+export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || "FlexTest";
+export const MOCKARO_KEY = process.env.NEXT_PUBLIC_MOCKARO_KEY
